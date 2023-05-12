e6e29b5267e93ce769363698cd99e383fc8645bb
Wilmar Caicedo <wilmar.caicedo@farmappweb.com>
2023-05-11 22:49:40 -0500

    add docker configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RideModule } from './ride/ride.module';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [],
+  imports: [RideModule, PaymentModule],
   controllers: [AppController],
   providers: [AppService],
 })
